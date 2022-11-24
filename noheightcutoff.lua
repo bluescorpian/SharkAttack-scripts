127050454904411cd3ee2136dd854a9e6055c3ab
bluescorpian <dsharryh27@gmail.com>
2022-11-24 14:47:18 +0200

    No Height Cutoff

---
+++ noheightcutoff.lua
@@ -0,0 +1,11 @@
+-- local utils = loadstring(game:HttpGet("https://raw.githubusercontent.com/bluescorpian/missingPieceUtils/main/missingPieceUtils.lua"))()
+local startBoat = getsenv(game:GetService("Players").LocalPlayer.PlayerScripts.Boats.BoatController).startBoat
+
+local oldStartBoat
+oldStartBoat = hookfunction(startBoat, function(boat, ...)
+    -- utils.rconsoleprint(config)
+    local config = boat.Configuration
+    config.Engine.EngineCutOutHeight.Value = 50
+    
+    return oldStartBoat(boat, ...)
+end)
